30e79afdc49c8fc924497c2ad889c23725a7e779
walk8243 <walk8243@gmail.com>
2021-08-06 07:24:54 +0900

    ターミナル内のクリアする設定を調整

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { terminal } from 'terminal-kit';
 
-terminal( "My name is " ).red( "Jack" )( " and I'm " ).green( "32\n" ) ;
+terminal.scrollUp(terminal.height + 1);
+terminal.clear();
+terminal("My name is ").green("walk8243");
+
+setTimeout(() => {
+	terminal.clear();
+	terminal.bold.bgWhite.black("My name is walk8243");
+}, 2 * 1000);
